52019ebaea9407b1161331cac5a0b133b06b7057
Martin Pribyl <martin.pribyl@hyperdigital.de>
2022-11-29 09:24:13 +0100

    Bugfix of USE in ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
-
-use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
-use TYPO3\CMS\Core\Core\Environment;
+// USE cannot be used, because of duplicated namings in cached files
+//use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+//use TYPO3\CMS\Core\Core\Environment;
 
 defined('TYPO3') or die();
 
