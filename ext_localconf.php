cd00956a35f49fce73b7b47c0d77fd659dbc2e3c
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-08-16 09:59:28 +0200

    task: migrate icons to new icon api

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,21 +6,6 @@
 defined('TYPO3') or die();
 
 (function () {
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Imaging\IconRegistry::class
-    );
-
-    $iconRegistry->registerIcon(
-        'hd_translator_icon',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:hd_translator/Resources/Public/Icons/typo3_icon_lang-switch.svg']
-    );
-    $iconRegistry->registerIcon(
-        'hd_translator_icon_doc_header',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:hd_translator/Resources/Public/Icons/typo3_icon_lang-switch_docheader.svg']
-    );
-
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Backend\Template\Components\ButtonBar']['getButtonsHook'][] =
         \Hyperdigital\HdTranslator\Hooks\DocHeaderButtonsHook::class . '->addExportButton';
 })();
